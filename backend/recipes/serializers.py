ebff66b905dc606a23a3ed5c83e2c2108efa23a5
Мария Кляхина <mr1.km@yandex.ru>
2023-04-01 10:41:09 +0300

    Дипломный проект: сайт Foodgram 5.0

--- backend/recipes/serializers.py
+++ backend/recipes/serializers.py
@@ -143,7 +143,7 @@ class RecipeSerializer(ModelSerializer):
     def validate(self, data):
         ingredients = self.initial_data.get('ingredients')
         tags = self.initial_data.get('tags')
-        cooking_time = self.validated_data.get('cooking_time')
+        cooking_time = data.get('cooking_time')
         if not ingredients:
             raise ValidationError(
                 {'ingredients': ('В рецепт необходимо добавить '
