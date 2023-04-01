8ca5fe5243c8353066e49c56236890d2dea91dbc
Мария Кляхина <mr1.km@yandex.ru>
2023-04-01 16:31:30 +0300

    Дипломный проект: сайт Foodgram 5.0

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['51.250.79.66', 'localhost', 'foodgram-recipes.sytes.net', 'backend']
 
