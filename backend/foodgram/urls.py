551a9a1407e4a3df607ac4c119e3a604f357e3a9
Мария Кляхина <mr1.km@yandex.ru>
2023-04-02 16:32:07 +0300

    Дипломный проект: сайт Foodgram 6.0

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 
 from recipes.views import IngredientViewSet, RecipeViewSet, TagViewSet
@@ -15,6 +16,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('users.urls')),
+    path(
+        'api/docs/redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 
 if settings.DEBUG:
